9188ec36cb1b1007ef2c2ebccdf1941f624cd2c6
andrewzm <andrewzm@gmail.com>
2016-01-21 17:57:22 +1100

    Put covr back in Suggests

--- tests/testthat/test_sre_polygon_predict.R
+++ tests/testthat/test_sre_polygon_predict.R
@@ -14,17 +14,14 @@ test_that("can predict over polygons in plane", {
     HexPols <- HexPoints2SpatialPolygons(HexPts)
     HexPts_df <- SpatialPointsDataFrame(HexPts,data.frame(fs=rep(1,length(HexPts))))
     HexPols_df <- SpatialPolygonsDataFrame(HexPols,
-                                            over(HexPols,meuse) %>%
-                                                select(zinc)) %>%
-                    subset(!is.na(zinc))
-
+                                           over(HexPols,meuse.grid))
 
     G <- auto_basis(m = plane(),data=meuse,nres = 2,prune=10,type = "Gaussian")
 
     S1 <- eval_basis(G,HexPts_df)
     S2 <- eval_basis(G,HexPols_df)
-    plot(as.numeric(S1))
-    lines(as.numeric(S2),col='red')
+    #plot(as.numeric(S1))
+    #lines(as.numeric(S2),col='red')
 
     HexPols_df <- auto_BAUs(manifold = plane(),
                             cellsize = c(400,400),
@@ -34,8 +31,8 @@ test_that("can predict over polygons in plane", {
     HexPols_df$fs <- 1
     f <- log(zinc) ~ 1
     S <- SRE(f,data = list(meuse),basis = G,BAUs = HexPols_df,est_error = FALSE)
-    S <- SRE.fit(S,n_EM = 10,print_lik=T)
+    S <- SRE.fit(S,n_EM = 10,print_lik=F)
     HexPols_df <- SRE.predict(S,HexPols_df,use_centroid = FALSE)
-    spplot(HexPols_df,"mu")
+    #spplot(HexPols_df,"mu")
 })
 
