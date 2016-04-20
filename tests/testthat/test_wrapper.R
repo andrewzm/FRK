aac63ad1d81764a03895342b92e0a947674b9f99
andrewzm <andrewzm@gmail.com>
2016-04-20 14:01:29 +1000

    Fixed wrapper bug

--- tests/testthat/test_wrapper.R
+++ tests/testthat/test_wrapper.R
@@ -5,10 +5,10 @@ test_that("basic_wrapper",{
     Z <- data.frame(x = runif(1000), y= runif(1000))
     Z$z <- sin(8*Z$x) + cos(8*Z$y) + 0.5*rnorm(100)
     coordinates(Z) = ~x+y
-    Pred <- FRK(f = z~1,
+    S <- FRK(f = z~1,
                 list(Z),
-                cellsize = c(0.02,0.02),
-                n_EM = 100)
-    spplot(Z,"z")
-    spplot(Pred$PredPolys,"mu")
+                cellsize = c(0.1,0.1),
+                n_EM = 2)
+    Pred <- SRE.predict(SRE_model = S,
+                        obs_fs = TRUE)
 })
